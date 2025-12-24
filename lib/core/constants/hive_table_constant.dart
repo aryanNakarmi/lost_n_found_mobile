08ad707d249a68d5f762aa6bcb20b2516126541c
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-24 10:36:43 +0545

    hive table database name and failure

---
+++ lib/core/constants/hive_table_constant.dart
@@ -0,0 +1,23 @@
+class HiveTableConstant{
+  HiveTableConstant._();
+
+  //Database name
+  static const String dbName = 'lost_nfound-db';
+  
+  static const int batchTypeId = 0;
+  static const String batchTable ='batch_table';
+  
+  static const int studentTypeId = 1;
+  static const String studentTable ='batch_table';
+  
+  static const int itemTypeId = 2;
+  static const String itemTable ='batch_table';
+  
+  static const int categoryTypeId = 3;
+  static const String categoryTable ='batch_table';
+  
+  static const int commentsTypeId = 4;
+  static const String commentsTable ='batch_table';
+  
+  
+} 
