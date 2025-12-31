a80db18203fd086a25eb853c22de849abe51ffcc
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-31 14:40:16 +0545

    create batch usecase

--- lib/features/batch/domain/usecases/create_batch_usecase.dart
+++ lib/features/batch/domain/usecases/create_batch_usecase.dart
@@ -1,9 +1,13 @@
 import 'package:dartz/dartz.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lost_n_found/core/error/failures.dart';
 import 'package:lost_n_found/core/usecases/app_usecase.dart';
+import 'package:lost_n_found/features/batch/data/repositories/batch_repositories.dart';
 import 'package:lost_n_found/features/batch/domain/entities/batch_entity.dart';
 import 'package:lost_n_found/features/batch/domain/repositories/batch_repository.dart';
+
+
 
 class CreateBatchUsecaseParams extends Equatable {
   final String batchName;
@@ -14,8 +18,14 @@ class CreateBatchUsecaseParams extends Equatable {
   List<Object?> get props => [batchName];
 }
 
-class CreateBatchUsecase implements UsecaseWithParams<void,CreateBatchUsecaseParams>{
+final CreateBatchUsecaseProviider = Provider<CreateBatchUsecase>((ref){
+  return CreateBatchUsecase(ref.read(batchRepositoryProvider));
+});
+
+class CreateBatchUsecase 
+implements UsecaseWithParams<void,CreateBatchUsecaseParams>{
   final IBatchRepository _batchRepository;
+  
   CreateBatchUsecase(this._batchRepository);
   @override
   Future<Either<Failure, void>> call(CreateBatchUsecaseParams params) {
