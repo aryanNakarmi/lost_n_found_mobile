b5cc974c1ca2b0a5b3a7f9461b32a1c24493236d
Aryan <aaryan.nakarmi111@gmail.com>
2025-12-31 10:40:00 +0545

    Local datasource provider created

--- lib/features/batch/data/datasources/local/local_batch_datasource.dart
+++ lib/features/batch/data/datasources/local/local_batch_datasource.dart
@@ -1,8 +1,14 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lost_n_found/core/services/hive/hive_service.dart';
 import 'package:lost_n_found/features/batch/data/datasources/batch_datasource.dart';
 import 'package:lost_n_found/features/batch/data/models/batch_hive_model.dart';
 
+final BatchLocalDatasourceProvider = Provider<BatchLocalDatasource>((ref){
+  return BatchLocalDatasource(hiveService: ref.read(hiveServiceProvider));
+});
+
 class BatchLocalDatasource implements IBatchDataSource {
+  //dependency injection
   final HiveService _hiveService;
 
   BatchLocalDatasource({required HiveService hiveService}):_hiveService = hiveService;
