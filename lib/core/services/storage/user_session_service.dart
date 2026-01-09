7e1c39595c379896b49c9f97eccb0ce0a621560b
Aryan <aaryan.nakarmi111@gmail.com>
2026-01-09 20:43:15 +0545

    clear usersession

--- lib/core/services/storage/user_session_service.dart
+++ lib/core/services/storage/user_session_service.dart
@@ -25,8 +25,51 @@ class UserSesssionService {
   static const String _keysIsLoggedIn = 'is_logged_in';
   static const String _keyUserId = 'user_id';
   static const String _keyUserEmail = 'user_email';
+  static const String _keyUsername = 'username';
   static const String _keyUserFullName = 'user_full_name';
   static const String _keyUserPhoneNumber = 'user_phone_number';
   static const String _keyUserBatchId= 'user_batch_id';
   static const String _keyUserProfileImage= 'user_profile_image';
+
+
+  //store user session data
+  Future<void> saveUserSession({
+    required String userId,
+    required String email,
+    required String username,
+    required String fullName,
+    required String? phoneNumber,
+    required String? batchId,
+    String? profileImage,
+    
+  }) async {
+    await _pref.setBool(_keysIsLoggedIn, true);
+    await _pref.setString(_keyUserId, userId);
+    await _pref.setString(_keyUserEmail, email);
+    await _pref.setString(_keyUsername, username);
+    await _pref.setString(_keyUserFullName, fullName);
+    if( phoneNumber != null){
+    await _pref.setString(_keyUserPhoneNumber, phoneNumber);
+    }
+    if( batchId != null){
+    await _pref.setString(_keyUserBatchId, batchId);
+    }
+    if( profileImage != null){
+    await _pref.setString(_keyUserProfileImage, profileImage);
+    }
+  }
+
+
+  //clear user session data
+  Future<void> clearUserSession() async {
+    await _pref.remove(_keyUserBatchId);
+    await _pref.remove(_keyUserPhoneNumber);
+    await _pref.remove(_keyUserFullName);
+    await _pref.remove(_keyUsername);
+    await _pref.remove(_keyUserEmail);
+    await _pref.remove(_keyUserId);
+    await _pref.remove(_keysIsLoggedIn);
+    await _pref.remove(_keyUserProfileImage);
+  }
+
 }
